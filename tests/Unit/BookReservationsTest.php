f4b2e47411a9a61dc433e9919814981aba61f0e5
farshad vahidifard <farshad.vahidifard@studenti.unimi.it>
2021-02-16 11:59:19 +0100

    RED: check in test fail

--- tests/Unit/BookReservationsTest.php
+++ tests/Unit/BookReservationsTest.php
@@ -29,4 +29,21 @@ class BookReservationsTest extends TestCase
         $this->assertEquals($book->id, Reservation::first()->book_id);
         $this->assertEquals(now(), Reservation::first()->checked_out_at);
     }
+
+    /** @test */
+    public function a_book_can_be_returned()
+    {
+        $book = Book::factory()->create();
+        $user = User::factory()->create();
+        $book->checkout($user);
+
+        $book->checkin($user);
+
+        $this->assertCount(1, Reservation::all());
+        $this->assertEquals($user->id, Reservation::first()->user_id);
+        $this->assertEquals($book->id, Reservation::first()->book_id);
+        $this->assertEquals(now(), Reservation::first()->checked_in_at);
+    }
+
+    //if not checked out, then exception
 }
