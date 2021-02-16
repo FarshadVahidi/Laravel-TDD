ec02034d3f437e0a91dc183404034fd922fe6d0d
farshad vahidifard <farshad.vahidifard@studenti.unimi.it>
2021-02-16 18:35:00 +0100

    GREEN: just login user can checkin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BooksController;
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\CheckinBookController;
 use App\Http\Controllers\CheckoutBookController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,6 +28,7 @@ Route::delete('/books/{book}', [BooksController::class, 'destroy']);
 Route::post('/author', [AuthorController::class, 'store']);
 
 Route::post('/checkout/{book}', [CheckoutBookController::class, 'store']);
+Route::post('/checkin/{book}',  [CheckinBookController::class, 'store']);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
