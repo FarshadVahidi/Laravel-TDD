77835ec6cb907f84b5d90e067bb59460dac4c2f4
farshad vahidifard <farshad.vahidifard@studenti.unimi.it>
2021-02-16 12:17:33 +0100

    GREEN: direct green on add twice check out for one user

--- tests/Unit/BookReservationsTest.php
+++ tests/Unit/BookReservationsTest.php
@@ -46,5 +46,34 @@ class BookReservationsTest extends TestCase
         $this->assertEquals(now(), Reservation::first()->checked_in_at);
     }
 
+    //a user can checked out a book twice
+    /** @test */
+    public function a_user_can_check_out_a_book_twice()
+    {
+        $book = Book::factory()->create();
+        $user = User::factory()->create();
+        $book->checkout($user);
+
+        $book->checkin($user);
+
+        $book->checkout($user);
+
+        $this->assertCount(2, Reservation::all());
+        $this->assertEquals($user->id, Reservation::find(2)->user_id);
+        $this->assertEquals($book->id, Reservation::find(2)->book_id);
+        $this->assertNull(Reservation::find(2)->checked_in_at);
+        $this->assertEquals(now(), Reservation::find(2)->checked_out_at);
+
+        $book->checkin($user);
+
+        $this->assertCount(2, Reservation::all());
+        $this->assertEquals($user->id, Reservation::find(2)->user_id);
+        $this->assertEquals($book->id, Reservation::find(2)->book_id);
+        $this->assertNotNull(Reservation::find(2)->checked_in_at);
+        $this->assertEquals(now(), Reservation::find(2)->checked_in_at);
+
+
+    }
+
     //if not checked out, then exception
 }
