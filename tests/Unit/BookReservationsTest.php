6f6e94ebb874e2b1f4f5858f0f0870cf5e2172f9
farshad vahidifard <farshad.vahidifard@studenti.unimi.it>
2021-02-16 11:08:15 +0100

    RED: raservation book fail

---
+++ tests/Unit/BookReservationsTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Reservation;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\Book;
+
+class BookReservationsTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_book_can_be_checked_out()
+    {
+        //$book->checkout($user);   or   //$user->checkout($book);
+        //I start with first one
+
+        $book = Book::factory()->create();
+        $user = User::factory()->create();
+
+        $book->checkout($user);
+
+        $this->assertCount(1, Reservation::all());
+        $this->assertEquals($user->id, Reservation::first()->user_id);
+        $this->assertEquals($book->id, Reservation::first()->book_id);
+        $this->assertEquals(now(), Reservation::first()->checked_out_at);
+    }
+}
